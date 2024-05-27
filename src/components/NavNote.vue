44c519f43d93093c24464ef0ee7d62553b4ad05a
Pablo Marino <pmarino2013@gmail.com.com>
2024-05-26 23:29:20 -0300

    Agrego font-awesome y mejoro los botones

--- src/components/NavNote.vue
+++ src/components/NavNote.vue
@@ -10,12 +10,14 @@ const props = defineProps({
 </script>
 <template>
   <div class="md:w-1/4 bg-white shadow-lg rounded-lg p-4 mb-4 md:mb-0">
-    <h2 class="text-xl font-semibold mb-4">Notas</h2>
     <button
       class="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mb-3"
       @click="clearNote"
     >
-      Nueva Nota
+      <div class="flex items-center justify-center gap-2">
+        <i class="fa fa-plus-square-o fa-2x" aria-hidden="true"></i
+        ><span class="text-xl">Nota nueva</span>
+      </div>
     </button>
     <ul>
       <li
@@ -24,16 +26,18 @@ const props = defineProps({
       >
         <div
           @click="readNote(item)"
-          class="flex-auto mb-2 p-2 bg-gray-200 rounded-l-lg cursor-pointer hover:bg-gray-300"
+          class="flex-auto mb-2 px-2 py-4 bg-gray-200 rounded-l-lg cursor-pointer hover:bg-gray-300"
         >
           <!-- <span>Nota {{ index + 1 }}</span> -->
-          <span>{{ item.text.substring(0, 20) }}...</span>
+          <span v-if="item.text.length > 20" class="text-xl"
+            >{{ item.text.substring(0, 20) }}...</span
+          ><span v-else class="text-xl">{{ item.text }}</span>
         </div>
         <div
-          class="mb-2 p-2 rounded-r-lg cursor-pointer hover:bg-gray-300 bg-red-500 text-white font-bold"
+          class="mb-2 px-2 py-3 rounded-r-lg cursor-pointer hover:bg-gray-300 bg-red-500 text-white font-bold"
           @click="deleteNote(item.id)"
         >
-          X
+          <i class="fa fa-trash-o fa-2x" aria-hidden="true"></i>
         </div>
       </li>
     </ul>
