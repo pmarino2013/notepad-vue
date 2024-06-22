20ae8529d668f7ff777f3bfb3af1c5a4e59b3184
Pablo Marino <pmarino2013@gmail.com.com>
2024-06-22 17:40:37 -0300

    Creo el archivo registerSW.js

--- src/main.js
+++ src/main.js
@@ -7,18 +7,18 @@ import "font-awesome/css/font-awesome.css";
 import App from "./App.vue";
 
 //PWA
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registrado con éxito:", registration);
-      })
-      .catch((error) => {
-        console.log("Error al registrar el Service Worker:", error);
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         console.log("Service Worker registrado con éxito:", registration);
+//       })
+//       .catch((error) => {
+//         console.log("Error al registrar el Service Worker:", error);
+//       });
+//   });
+// }
 //------------------------------------------
 
 createApp(App).use(router).mount("#app");
